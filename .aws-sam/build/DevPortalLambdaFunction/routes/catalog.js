556a6d1eb0e3e62fe9fcf3b7eff5b54f2ec6a334
Samuel Vange <7166607+svange@users.noreply.github.com>
2025-07-29 15:24:29 -0700

    chore: update .gitignore and prod.yaml for deployment configuration

--- .aws-sam/build/DevPortalLambdaFunction/routes/catalog.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const util = require('../util')
-
-exports.get = async (req, res) => {
-  console.log(`GET /catalog for Cognito ID: ${util.getCognitoIdentityId(req)}`)
-  const catalog = await util.catalog()
-  res.status(200).json(catalog)
-}
